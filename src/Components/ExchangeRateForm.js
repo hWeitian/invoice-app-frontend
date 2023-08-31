64c888be50f6e36c65c2d34ade1fbb1359eb5f8f
hWeitian <huangweitian1@hotmail.com>
2023-08-31 17:20:00 +0800

    Add exchange rates table and form

---
+++ src/Components/ExchangeRateForm.js
@@ -0,0 +1,190 @@
+import React, { useEffect, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Grid, Button, Typography, TextField } from "@mui/material";
+import DatePickerInput from "./DatePickerInput";
+import AutocompleteInput from "./AutocompleteInput";
+import dayjs from "dayjs";
+import useGetAccessToken from "../Hooks/useGetAccessToken";
+import { useOutletContext } from "react-router-dom";
+import axios from "axios";
+import { formatDate } from "../utils";
+
+const ExchangeRateForm = ({
+  setOpenForm,
+  data,
+  setSelectedRow,
+  getExchangeRates,
+  resetSearch,
+  setResetSearch,
+}) => {
+  const [setOpenFeedback, setFeedbackMsg, setFeedbackSeverity] =
+    useOutletContext();
+  const [selectedId, setSelectedId] = useState(null);
+  const getAccessToken = useGetAccessToken();
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+    setValue,
+  } = useForm();
+
+  const onSubmit = (formData) => {
+    const dataToUpdate = {
+      rate: formData.rate,
+      date: formatDate(formData.date),
+    };
+    // console.log(dataToUpdate);
+    submitData(dataToUpdate);
+  };
+
+  useEffect(() => {
+    prefillData();
+  }, [data]);
+
+  const prefillData = () => {
+    if (data) {
+      setSelectedId(data.id);
+      setValue("rate", data.rate);
+      setValue("date", dayjs(new Date(data.date)));
+    } else {
+      reset();
+    }
+  };
+
+  const handleFormClose = () => {
+    setOpenForm(false);
+    setSelectedRow(null);
+    reset();
+    setSelectedId(null);
+  };
+
+  const updateIssueInDb = async (dataToUpdate) => {
+    try {
+      const accessToken = await getAccessToken();
+      const response = await axios.put(
+        `${process.env.REACT_APP_DB_SERVER}/magazines/${selectedId}`,
+        dataToUpdate,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+      return response.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const addNewRate = async (dataToUpdate) => {
+    try {
+      const accessToken = await getAccessToken();
+      const response = await axios.post(
+        `${process.env.REACT_APP_DB_SERVER}/exchange-rates/`,
+        dataToUpdate,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+      return response.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const submitData = async (dataToUpdate) => {
+    if (selectedId) {
+      await updateIssueInDb(dataToUpdate);
+      setFeedbackSeverity("success");
+      setFeedbackMsg(`${data.month} ${data.year} Issue Updated`);
+    } else {
+      await addNewRate(dataToUpdate);
+      setFeedbackSeverity("success");
+      setFeedbackMsg(`New Rate Added`);
+    }
+    getExchangeRates();
+    setResetSearch(!resetSearch);
+    setOpenFeedback(true);
+    handleFormClose();
+  };
+
+  return (
+    <>
+      <Typography sx={{ fontWeight: 700, fontSize: "1rem", mb: 2 }}>
+        {data ? `Editing ${data.date}` : "Add Rate"}
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container>
+          <Grid item xs={12}>
+            <label className="form-label">Date</label>
+            <Controller
+              control={control}
+              name="date"
+              defaultValue=""
+              rules={{ required: "Please select a date" }}
+              render={({
+                field: { ref, onChange, onBlur, ...field },
+                fieldState,
+              }) => (
+                <DatePickerInput
+                  error={errors.date?.message}
+                  value={field.value}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <label className="form-label">Rate</label>
+            <Controller
+              control={control}
+              name="rate"
+              rules={{ required: "Please enter a rate" }}
+              defaultValue=""
+              render={({
+                field: { ref, onChange, ...field },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  id={`rate`}
+                  variant="outlined"
+                  size="small"
+                  error={error}
+                  value={field.value}
+                  onChange={onChange}
+                  helperText={error?.message}
+                  placeholder="Rate"
+                  sx={{ width: "100%" }}
+                  type="number"
+                />
+              )}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid
+          container
+          sx={{
+            justifyContent: "flex-end",
+            position: "absolute",
+            bottom: "20px",
+            right: "20px",
+          }}
+        >
+          <Button
+            color="primary"
+            variant="outlined"
+            sx={{ mr: 2 }}
+            onClick={handleFormClose}
+          >
+            Close
+          </Button>
+          <Button type="submit" color="primary" variant="contained">
+            Save
+          </Button>
+        </Grid>
+      </form>
+    </>
+  );
+};
+
+export default ExchangeRateForm;
--- src/Pages/ExchangeRates.js
+++ src/Pages/ExchangeRates.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { Grid, Button, Box, Chip } from "@mui/material";
-import PageTitle from "../Components/PageTitle";
-import AddIcon from "@mui/icons-material/Add";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Button, Grid, Box, Paper, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { DataGrid } from "@mui/x-data-grid";
-import { createStringDate } from "../utils";
-import axios from "axios";
+import { useOutletContext } from "react-router-dom";
+import PageTitle from "../Components/PageTitle";
+import { getData, createStringDate } from "../utils";
 import useGetAccessToken from "../Hooks/useGetAccessToken";
+import ExchangeRateForm from "../Components/ExchangeRateForm";
+import axios from "axios";
+import ConfirmationModal from "../Components/ConfirmationModal";
 import SearchBar from "../Components/SearchBar";
 import AutocompleteInput from "../Components/AutocompleteInput";
-import { getData } from "../utils";
-import TableMenu from "../Components/TableMenu";
 
 const ExchangeRates = () => {
   const defaultPage = 0;
   const defaultPageSize = 10;
-  const navigate = useNavigate();
+  const [setOpenFeedback, setFeedbackMsg, setFeedbackSeverity] =
+    useOutletContext();
   const getAccessToken = useGetAccessToken();
-  const [invoices, setInvoices] = useState();
-  const [resetSearch, setResetSearch] = useState(false);
-  const [selectedSearchOption, setSelectedSearchOption] = useState({
-    name: "Search Company",
-    type: "text",
-    id: 1,
-  });
+  const [openConfirmation, setOpenConfirmation] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [exchangeRates, setExchangeRates] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [resetSearch, setResetSearch] = useState(false);
+  const [selectedSearchOption, setSelectedSearchOption] = useState({
+    name: "Search Year",
+    id: 1,
+    type: "number",
+  });
   const [paginationModel, setPaginationModel] = useState({
     page: defaultPage,
     pageSize: defaultPageSize,
@@ -37,43 +42,74 @@ const ExchangeRates = () => {
 
   useEffect(() => {
     if (searchValue.length <= 0) {
-      getInvoices();
+      getExchangeRates();
     } else {
-      searchInvoicesAfterPageChange();
+      searchMagazinesAfterPageChange();
     }
   }, [paginationModel]);
 
-  const getInvoices = async () => {
+  const getExchangeRates = async () => {
     try {
       setIsLoading(true);
       const accessToken = await getAccessToken();
-      const response = await axios.get(
-        `${process.env.REACT_APP_DB_SERVER}/invoices/table-data/${paginationModel.page}/${paginationModel.pageSize}`,
-        {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        }
+      const response = await getData(
+        accessToken,
+        `exchange-rates/${paginationModel.page}/${paginationModel.pageSize}`
       );
-      setTotalPages(response.data.count);
-      setInvoices(response.data.rows);
+      console.log(response);
+      setTotalPages(response.count);
+      setExchangeRates(response.rows);
       setIsLoading(false);
     } catch (e) {
       console.log(e);
     }
   };
 
-  const searchInvoices = async () => {
+  const handleClick = () => {
+    setOpenForm(true);
+  };
+
+  const handleDelete = async () => {
+    setOpenConfirmation(false);
+    try {
+      const accessToken = await getAccessToken();
+      const response = await axios.delete(
+        `${process.env.REACT_APP_DB_SERVER}/magazines/${selectedRow.id}`,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+
+      await getExchangeRates();
+      setSelectedRow(null);
+      setFeedbackSeverity("success");
+      setFeedbackMsg("Issue deleted");
+      setOpenFeedback(true);
+    } catch (e) {
+      setSelectedRow(null);
+      setFeedbackSeverity("error");
+      setFeedbackMsg(
+        "Unable to delete. An invoice or insertion order has been created for this issue."
+      );
+      setOpenFeedback(true);
+      console.log(e);
+    }
+    setResetSearch(!resetSearch);
+  };
+
+  const searchMagazines = async () => {
     let data;
     try {
       const accessToken = await getAccessToken();
-      if (selectedSearchOption.name === "Search Company") {
+      if (selectedSearchOption.name === "Search Year") {
         data = await getData(
           accessToken,
-          `invoices/search/company/${searchValue}/${defaultPage}/${defaultPageSize}`
+          `magazines/search/year/${searchValue}/${defaultPage}/${defaultPageSize}`
         );
       } else {
         data = await getData(
           accessToken,
-          `invoices/search/id/${searchValue}/${defaultPage}/${defaultPageSize}`
+          `magazines/search/month/${searchValue}/${defaultPage}/${defaultPageSize}`
         );
       }
       setPaginationModel({
@@ -81,43 +117,32 @@ const ExchangeRates = () => {
         pageSize: defaultPageSize,
       });
       setTotalPages(data.count);
-      setInvoices(data.rows);
+      setExchangeRates(data.rows);
     } catch (e) {
       console.log(e);
     }
   };
 
-  const searchInvoicesAfterPageChange = async () => {
+  const searchMagazinesAfterPageChange = async () => {
     let data;
     try {
       const accessToken = await getAccessToken();
-      if (selectedSearchOption.name === "Search Company") {
+      if (selectedSearchOption.name === "Search Year") {
         data = await getData(
           accessToken,
-          `invoices/search/company/${searchValue}/${paginationModel.page}/${paginationModel.pageSize}`
+          `magazines/search/year/${searchValue}/${paginationModel.page}/${paginationModel.pageSize}`
         );
       } else {
         data = await getData(
           accessToken,
-          `invoices/search/id/${searchValue}/${paginationModel.page}/${paginationModel.pageSize}`
+          `magazines/search/month/${searchValue}/${paginationModel.page}/${paginationModel.pageSize}`
         );
       }
       setTotalPages(data.count);
-      setInvoices(data.rows);
+      setExchangeRates(data.rows);
     } catch (e) {
       console.log(e);
     }
-  };
-
-  const currencyFormatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-
-  const usdPrice = {
-    type: "number",
-    valueFormatter: ({ value }) => currencyFormatter.format(value),
-    cellClassName: "font-tabular-nums",
   };
 
   const clearSearch = () => {
@@ -125,119 +150,66 @@ const ExchangeRates = () => {
       page: defaultPage,
       pageSize: defaultPageSize,
     });
-    getInvoices();
+    getExchangeRates();
   };
 
   const searchOptions = [
     {
-      name: "Search Company",
-      type: "text",
+      name: "Search Year",
+      type: "number",
       id: 1,
     },
     {
-      name: "Search Invoice Number",
-      type: "number",
+      name: "Search Month",
+      type: "text",
       id: 2,
     },
   ];
 
-  if (invoices) {
+  if (exchangeRates) {
     columns = [
-      { field: "id", headerName: "Invoice #", width: 100 },
-      { field: "date", headerName: "Date", width: 130 },
+      { field: "date", headerName: "Date", width: 180 },
+      { field: "rate", headerName: "Rate", width: 400 },
       {
-        field: "status",
-        headerName: "Status",
-        width: 150,
-        renderCell: (params) => {
-          const cellValue = params.row["status"];
-          let output = "";
-          let color = "";
-          if (cellValue === "Paid") {
-            output = "Paid";
-            color = "chipGreen";
-          } else if (cellValue === "Partial Paid") {
-            output = "Partial Paid";
-            color = "chipOrange";
-          } else {
-            output = "Pending";
-            color = "chipRed";
-          }
-          return <Chip label={output} color={color} size="small"></Chip>;
-        },
-      },
-      {
-        field: "company",
-        headerName: "Company",
-        width: 290,
-      },
-      {
-        field: "totalAmount",
-        headerName: "Amount",
-        width: 160,
-        ...usdPrice,
-        align: "left",
-        headerAlign: "left",
-      },
-      {
-        field: "outstanding",
-        headerName: "Outstanding Amount",
-        width: 190,
-        ...usdPrice,
-        align: "left",
-        headerAlign: "left",
-      },
-      {
-        field: "url",
-        headerName: "File",
-        width: 90,
+        field: "delete",
+        headerName: "",
+        width: 50,
         sortable: false,
-        disableClickEventBubbling: true,
         renderCell: (params) => {
-          const onClick = (e) => {
-            const url = params.row["url"];
-            window.open(url, "_blank");
+          const deleteClicked = () => {
+            setOpenConfirmation(true);
+            setSelectedRow(params.row);
           };
-
           return (
-            <Button
-              variant="text"
-              color="secondary"
-              size="small"
-              onClick={onClick}
-            >
-              Download
-            </Button>
+            <IconButton onClick={deleteClicked}>
+              <DeleteIcon sx={{ fontSize: 21 }} />
+            </IconButton>
           );
         },
       },
       {
-        field: "more",
+        field: "edit",
         headerName: "",
-        width: 90,
+        width: 50,
         sortable: false,
-        disableClickEventBubbling: true,
-        renderCell: (params) => (
-          <TableMenu
-            rowData={params.row}
-            getInvoices={getInvoices}
-            resetSearch={resetSearch}
-            setResetSearch={setResetSearch}
-          />
-        ),
+        renderCell: (params) => {
+          const onClick = () => {
+            setOpenForm(true);
+            setSelectedRow(params.row);
+          };
+          return (
+            <IconButton onClick={onClick}>
+              <EditIcon sx={{ fontSize: 21 }} />
+            </IconButton>
+          );
+        },
       },
     ];
 
-    rows = invoices.map((invoice, index) => ({
-      id: `${invoice.id}.INV`,
-      date: createStringDate(invoice.invoiceDate),
-      status: invoice.status,
-      company: invoice.company.name,
-      totalAmount: invoice.totalAmount,
-      outstanding: invoice.outstanding,
-      url: invoice.url,
-      //Data pass in but will not be shown. Use to update payment.
-      companyId: invoice.company.id,
+    rows = exchangeRates.map((rate, index) => ({
+      date: createStringDate(rate.date),
+      rate: rate.rate,
+      id: rate.id,
     }));
   }
 
@@ -252,23 +224,26 @@ const ExchangeRates = () => {
               width: "100%",
             }}
           >
-            <PageTitle>Invoices</PageTitle>
+            <PageTitle>Exchange Rates</PageTitle>
           </Box>
         </Grid>
-        <Grid item xs={12} sx={{ mt: 0 }}>
-          <div style={{ width: "100%" }}>
+        <Grid
+          container
+          sx={{ justifyContent: "space-between", minHeight: "583px" }}
+        >
+          <Grid item xs={7} sx={{ mt: 0 }}>
             <Grid container sx={{ mb: 2, width: "100%" }}>
-              <Grid item sx={{ mr: 1 }} xs={3}>
+              <Grid item sx={{ mr: 1 }} xs={7.4}>
                 <SearchBar
                   searchValue={searchValue}
                   setSearchValue={setSearchValue}
-                  search={searchInvoices}
+                  search={searchMagazines}
                   clearSearch={clearSearch}
-                  selectedSearchOption={selectedSearchOption}
                   resetSearch={resetSearch}
+                  selectedSearchOption={selectedSearchOption}
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item>
                 <AutocompleteInput
                   id="magazine-input"
                   placeholder="Select an issue"
@@ -283,41 +258,93 @@ const ExchangeRates = () => {
                   width="260px"
                 />
               </Grid>
-              <Grid
-                item
-                sx={{
-                  textAlign: "right",
-                }}
-                xs={2.9}
-              >
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="small"
-                  onClick={() => navigate("/add-invoice")}
-                >
-                  <AddIcon sx={{ mr: 1 }} />
-                  Create Invoice
-                </Button>
-              </Grid>
             </Grid>
-            <DataGrid
-              autoHeight
-              disableRowSelectionOnClick
-              rows={rows}
-              columns={columns}
-              pageSizeOptions={[5, 10]}
-              disableColumnFilter
-              disableColumnMenu
-              paginationMode="server"
-              rowCount={totalPages}
-              paginationModel={paginationModel}
-              onPaginationModelChange={setPaginationModel}
-              loading={isLoading}
-            />
-          </div>
+            <Grid container>
+              <DataGrid
+                disableRowSelectionOnClick
+                autoHeight
+                rows={rows}
+                columns={columns}
+                pageSizeOptions={[5, 10]}
+                disableColumnFilter
+                disableColumnMenu
+                paginationMode="server"
+                rowCount={totalPages}
+                paginationModel={paginationModel}
+                onPaginationModelChange={setPaginationModel}
+                loading={isLoading}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={4.7}
+            sx={{ mt: 0, position: "relative", height: "100%" }}
+          >
+            <Paper
+              sx={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#FFFFFF",
+                p: 2,
+              }}
+            >
+              {openForm ? (
+                <div style={{ minHeight: "490px" }}>
+                  <ExchangeRateForm
+                    setOpenForm={setOpenForm}
+                    data={selectedRow}
+                    setSelectedRow={setSelectedRow}
+                    getExchangeRates={getExchangeRates}
+                    resetSearch={resetSearch}
+                    setResetSearch={setResetSearch}
+                  />
+                </div>
+              ) : (
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    alignContent: "center",
+                    height: "100%",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <p
+                    style={{
+                      width: "100%",
+                      textAlign: "center",
+                      padding: 0,
+                      margin: 0,
+                    }}
+                  >
+                    Select a rate to edit or
+                  </p>
+                  <div style={{ width: "100%", textAlign: "center" }}>
+                    <Button
+                      onClick={handleClick}
+                      color="secondary"
+                      variant="text"
+                      sx={{ textTransform: "none" }}
+                    >
+                      Click to add a new rate
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
+      <ConfirmationModal
+        open={openConfirmation}
+        setOpenConfirmation={setOpenConfirmation}
+        handleDelete={handleDelete}
+        title="Delete Issue"
+      >
+        Are you sure you want to delete this issue? This action cannot be
+        undone.
+      </ConfirmationModal>
     </>
   );
 };
